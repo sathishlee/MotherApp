95f0506f4d8b8c508b8b9488d2b7ce5c3623d0c4
sathish <sathish.s@satvatinfosol.com>
2018-02-21 15:45:31 +0530

    Immunization & PostnatalMotherVisit screen UI Done.

---
+++ app/src/main/java/com/unicef/thaimai/motherapp/activity/ImmunizationActivity.java
@@ -0,0 +1,15 @@
+package com.unicef.thaimai.motherapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.unicef.thaimai.motherapp.R;
+
+public class ImmunizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immunization);
+    }
+}
