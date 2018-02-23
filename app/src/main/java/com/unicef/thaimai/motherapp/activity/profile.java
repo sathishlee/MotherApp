eef918e8ebac2c7bf9d31f5b54774eb7a0230c9b
suthishan <suthishan@satvatinfosol.com>
2018-02-23 11:35:47 +0530

    Profile Updated

--- app/src/main/java/com/unicef/thaimai/motherapp/activity/profile.java
+++ app/src/main/java/com/unicef/thaimai/motherapp/activity/profile.java
@@ -1,44 +1,80 @@
 package com.unicef.thaimai.motherapp.activity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
 
 import com.unicef.thaimai.motherapp.R;
 
 
 public class profile extends AppCompatActivity {
 
-    public static final String UPLOAD_URL = "http://192.168.100.19/image/upload.php";
-    public static final String UPLOAD_KEY = "image";
+    public static final String TITLE = "Profile";
+    public static final String UPLOAD_IMAGE = "user_profile_photo";
     private static final int PICK_IMAGE_REQUEST = 1;
 
-    ImageButton profileImage;
+   CollapsingToolbarLayout toolbar_layout;
+
+    ImageView user_profile_photo;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivityTransitions();
         setContentView(R.layout.layout_profile);
 
-        profileImage = (ImageButton) findViewById(R.id.user_profile_photo);
+        ViewCompat.setTransitionName(findViewById(R.id.app_bar), UPLOAD_IMAGE);
+        supportPostponeEnterTransition();
 
-        profileImage.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile");
+        actionBar.setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String itemTitle = getIntent().getStringExtra(TITLE);
+
+        toolbar_layout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolbar_layout.setTitle(itemTitle);
+        toolbar_layout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                intent.setType("image/*");
-                startActivityForResult(intent, PICK_IMAGE_REQUEST);
-
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+
 
     }
 
@@ -49,28 +85,41 @@ public class profile extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-            case R.id.action_notification:
-                Toast.makeText(getApplicationContext(),"Notification Clicked",Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.action_help:
-                Toast.makeText(getApplicationContext(),"Help Menu",Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                super.onOptionsItemSelected(item);
-        }return true;
+        Intent intent = new Intent(profile.this, MainActivity.class);
+        finish();
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
 
+    }
+
+    private void initActivityTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            getWindow().setEnterTransition(transition);
+            getWindow().setReturnTransition(transition);
+        }
+    }
+
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.primary_dark);
+        int primary = getResources().getColor(R.color.primary);
+        toolbar_layout.setContentScrimColor(palette.getMutedColor(primary));
+        toolbar_layout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        updateBackground((FloatingActionButton) findViewById(R.id.fab), palette);
+        supportStartPostponedEnterTransition();
+    }
+
+    private void updateBackground(FloatingActionButton fab, Palette palette) {
+        int lightVibrantColor = palette.getLightVibrantColor(getResources().getColor(android.R.color.white));
+        int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.accent));
+
+        fab.setRippleColor(lightVibrantColor);
+        fab.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
     }
 
 }
