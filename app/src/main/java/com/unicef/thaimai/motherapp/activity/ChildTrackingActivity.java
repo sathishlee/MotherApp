e0a61d556a7321373c527c49f67f1208dddd0f3f
suthishan <suthishan@satvatinfosol.com>
2018-09-03 12:16:01 +0530

    Child Track

---
+++ app/src/main/java/com/unicef/thaimai/motherapp/activity/ChildTrackingActivity.java
@@ -0,0 +1,19 @@
+package com.unicef.thaimai.motherapp.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.unicef.thaimai.motherapp.R;
+
+/**
+ * Created by Suthishan on 20/1/2018.
+ */
+
+public class ChildTrackingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_tracking);
+    }
+}
